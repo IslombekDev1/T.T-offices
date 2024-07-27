22d0dc06326309a65ff83de2888c912d00c52055
IslombekDev1 <islomaliabdugaffarov@gmail.com>
2024-07-27 16:47:51 +0500

    Sergeli loc

--- src/components/pages/main/Main.jsx
+++ src/components/pages/main/Main.jsx
@@ -115,7 +115,7 @@ function Main() {
                             </p>
 
                             <div>
-                                <iframe title="This is a unique title" width={'100%'} src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Qo'rg'ontepa%20ko'chasi,%20Sergeli%20+(My%20Business%20Name)&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" />
+                                <iframe title="This is a unique title" width={'100%'} src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Qo'rg'ontepa%20ko'chasi,%20Sergeli,%20Astronaut%20game%20club%20vol%202.0%20+(My%20Business%20Name)&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" />
                             </div>
 
                             <div>
